369ce2824df6c492f19fc8b5f6dc1ad42fb6f5b8
Darshan D <darshand2000@gmail.com>
2021-02-26 00:37:13 +0530

    script

---
+++ run_script.sh
@@ -0,0 +1,6 @@
+python test.py < a.txt > a_o.txt
+python test.py < b.txt > b_o.txt
+python test.py < c.txt > c_o.txt
+python test.py < d.txt > d_o.txt
+python test.py < e.txt > e_o.txt
+python test.py < f.txt > f_o.txt
